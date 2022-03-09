6b49fe0e104c3d63cc8884d15ad4757beccc20eb
abdull-rahman <abed472011@mail.com>
2022-03-09 21:59:50 +0200

    initialized layout

--- src/app/layouts/main-layout/components/top-nav/top-nav.component.ts
+++ src/app/layouts/main-layout/components/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'main-top-nav',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public moveToLogin () {
+    this.router.navigate(['/login'])
+  }
 }
--- src/app/shared/main-module-imports.ts
+++ src/app/shared/main-module-imports.ts
@@ -1,11 +1,16 @@
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FooterComponent } from '../layouts/main-layout/components/footer/footer.component';
 
 /**
  * @description imports and declarations for the main layout declarations
  */
 export default {
     imports: [
-        MatButtonModule
+        MatButtonModule,
+        MatInputModule
     ],
-    declarations: []
+    declarations: [
+        FooterComponent
+    ]
 }
