ba03c398609b996f263bf28357dfa2938676f568
abdull-rahman <abed472011@mail.com>
2022-03-13 14:43:42 +0200

    updates

--- src/app/shared/main-module-imports.ts
+++ src/app/shared/main-module-imports.ts
@@ -1,6 +1,9 @@
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import { FooterComponent } from '../layouts/main-layout/components/footer/footer.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 /**
  * @description imports and declarations for the main layout declarations
@@ -8,7 +11,10 @@ import { FooterComponent } from '../layouts/main-layout/components/footer/footer
 export default {
     imports: [
         MatButtonModule,
-        MatInputModule
+        MatInputModule,
+        MatCheckboxModule,
+        MatInputModule,
+        MatFormFieldModule
     ],
     declarations: [
         FooterComponent
