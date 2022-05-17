20ab02dc81b35d82b2e41fa361522ae4c7ba4ccc
Abdull-Rahman <abed472011@gmail.com>
2022-05-17 23:37:47 +0300

    updates

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLayoutComponent } from 'src/app/layouts/user-layout/user-layout.component';
+import { DashboardComponent } from 'src/app/pages/user/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserLayoutComponent } from '../../layouts/user-layout/user-layout.component';
+import { DashboardComponent } from '../../pages/user/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [UserLayoutComponent],
+  declarations: [UserLayoutComponent, DashboardComponent],
   imports: [
     CommonModule,
     UserRoutingModule
