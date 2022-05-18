a9a1924f6712878d76c238ce8c3178978ad09d7f
Abdull Rahman <Sarvanan.prabhu@sphereit.me>
2022-05-18 20:49:17 +0300

    updates

--- src/app/layouts/user-layout/components/top-nav/top-nav.component.ts
+++ src/app/layouts/user-layout/components/top-nav/top-nav.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { LayoutUtilsService } from 'src/app/services/layout-utils.service';
 import { StoreService } from 'src/app/services/store.service';
 
 @Component({
@@ -7,20 +10,39 @@ import { StoreService } from 'src/app/services/store.service';
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.scss']
 })
-export class TopNavComponent implements OnInit {
+export class TopNavComponent implements OnInit, OnDestroy {
 
-  public currentUser = {}
+  public sideNavOpen = true;
+  public currentUser: any = {}
+  private subscriptions: Subscription[] = [];
   constructor(
     private auth: AuthService,
     private store: StoreService,
+    private layoutUtils: LayoutUtilsService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.loadSettings();
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+
   private loadSettings(){
     this.currentUser = this.auth.getAuthStatus().currentUser;
+    this.subscriptions.push(this.layoutUtils.getSidenavSub().subscribe(r => this.sideNavOpen = r))
+  }
+
+  public onSidenavToggle(){
+    if (this.sideNavOpen) return this.layoutUtils.closeSideNav()
+    this.layoutUtils.openSideNav();
+  }
+
+  public onLogout(){
+    this.auth.logout();
+    this.router.navigate(['/login']);
   }
 
 }
