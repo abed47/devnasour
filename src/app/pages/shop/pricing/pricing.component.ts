61f5961c81dca2fab4243fc260cbf07003351b1d
Abdull Rahman <Sarvanan.prabhu@sphereit.me>
2022-05-31 17:30:20 +0300

    updates

--- src/app/pages/shop/pricing/pricing.component.ts
+++ src/app/pages/shop/pricing/pricing.component.ts
@@ -8,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
 export class PricingComponent implements OnInit {
 
   public totalPrice = 0;
-  
+  public lidOptions = {
+    option1: true,
+    option2: false,
+    option3: false,
+    option4: false
+  }
+
+  public itemOptions = {
+    option1: true,
+    option2: false,
+    option3: false,
+    option4: false
+  }
+
+  public itemOptions2 = {
+    option1: true,
+    option2: false,
+    option3: false,
+    option4: false
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onLidOptionsChange(o){
+    if(o === null){
+      this.lidOptions = {
+        option1: false,
+        option2: false,
+        option3: false,
+        option4: false
+      }
+      return;
+    }
+
+    Object.keys(this.lidOptions).forEach((k, i)=> {
+      if(i === o) return this.lidOptions[k] = true;
+      this.lidOptions[k] = false;
+    })
   }
 
 }
