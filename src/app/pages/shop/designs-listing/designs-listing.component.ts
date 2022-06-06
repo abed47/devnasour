fa6885c7732447b04c3acf7f194ff641b4c05e8e
Abdull Rahman <Sarvanan.prabhu@sphereit.me>
2022-06-07 00:59:40 +0300

    updates

--- src/app/pages/shop/designs-listing/designs-listing.component.ts
+++ src/app/pages/shop/designs-listing/designs-listing.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignsListingComponent implements OnInit {
 
+  productList = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData() {
+    for (let i = 0; i < 26; i++) {
+      this.productList.push({
+        name: "test Prodsadfasfasfasfasfsafdasfasfasfasdfsadfasdfuct " + i,
+        rating: 3,
+        price: 25,
+        photo: "assets/images/box-image.png",
+        id: 1
+      })
+    }
   }
 
 }
