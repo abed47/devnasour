faccff9f8f404239ffcf88d2ca49cbf7838ff93f
Abdull-Rahman Al Haj Hassan <abdull-rahmanalhajhassan@Abdull-Rahmans-MacBook-Pro.local>
2022-09-14 23:03:37 +0400

    updates

--- src/app/pages/product-view/product-view.component.ts
+++ src/app/pages/product-view/product-view.component.ts
@@ -15,6 +15,7 @@ import { SwiperOptions } from 'swiper';
 export class ProductViewComponent implements OnInit, AfterViewChecked {
 
   public pathNameSubject: Subject<any> = new Subject();
+  public selectedColor: number = null;
   public selectedQuantity = 0;
   public selectedPrice = 0;
   public product = {
@@ -30,12 +31,11 @@ export class ProductViewComponent implements OnInit, AfterViewChecked {
     size: '',
     farmer: '',
     country: '',
-    sizing: ''
+    sizing: '',
+    color: [],
   };
 
-  public relatedProductsList = [
-
-  ]
+  public relatedProductsList = [];
 
   public relatedProductsSwiperConfig: SwiperOptions = {
     slidesPerView: 1,
@@ -43,12 +43,11 @@ export class ProductViewComponent implements OnInit, AfterViewChecked {
     // loop: true,
     // centeredSlides: true,
     // initialSlide: 3
-    
     // width: 500,
-  }
+  };
 
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private layoutUtilsService: LayoutUtilsService,
     private router: Router,
     private request: RequestService,
@@ -58,80 +57,85 @@ export class ProductViewComponent implements OnInit, AfterViewChecked {
   ngOnInit(): void {
     this.loadData();
   }
-  
+
   ngAfterViewChecked(): void {
   }
-  
-  private loadData(){
-    let productId = this.route.snapshot.params.id;
+
+  private loadData(): void{
+    const productId = this.route.snapshot.params.id;
     this.layoutUtilsService.renamePath('Product name', productId);
     this.layoutUtilsService.showLoader();
     this.relatedProductsList = [];
 
     this.request.getProductDetails(productId)
     .then(r => {
-      
-      if(r && r?.status === 1){
+
+      if (r && r?.status === 1){
+        console.log(r);
         this.product.name = r.data.web_product_name;
         this.product.images = r?.data?.attachments?.map(item => new ImageItem({src: item, thumb: item})) || [];
         this.product.description = r.data.web_product_description;
         this.product.overview = r.data.web_product_overview;
         this.product.price = r.data.web_product_price;
         this.product.discount = +r.data.web_product_discount;
-        //price opts
         this.product.priceList = r.data.quantity_pricing;
         this.selectedPrice = r.data.quantity_pricing[0].price;
         this.selectedQuantity = r.data.quantity_pricing[0].quantity;
-
+        this.product.color = r.data.product_color;
         this.product.shipping = r.data.web_product_shipping;
         this.product.sizing = r.data.web_product_sizing;
         this.product.size = r.data.web_product_size;
         this.product.farmer = r.data.web_product_farmer;
         this.product.country = r.data.web_country_name;
         this.product.rating = +r?.data?.web_product_rate + 1 || 0;
-        if(r?.data?.related_product) r.data.related_product.forEach(item => {
+        if (r?.data?.related_product) { r.data.related_product.forEach(item => {
           this.relatedProductsList.push({
             name: item.web_product_name,
             price: item.web_product_price,
             rating: +item.web_product_rate || 0,
             photo: item.attachments[0],
             id: item.web_product_id
-          })
-        })
+          });
+        });
+        }
       }
 
       this.layoutUtilsService.hidePreloader();
     })
     .catch(e => {
       this.layoutUtilsService.hidePreloader();
-    })
+    });
 
-    
+
   }
 
-  public handlePriceChange(e){
-    let newP = this.product.priceList.filter(item => item.quantity === e);
+  public handlePriceChange(e): void{
+    const newP = this.product.priceList.filter(item => item.quantity === e);
     this.selectedPrice = newP[0].price;
     this.selectedQuantity = newP[0].quantity;
   }
 
-  public navigateTo(e){
-    this.router.navigate(['/shop/product/' + e]).then( e => {
+  public navigateTo(e): void{
+    this.router.navigate(['/shop/product/' + e]).then( (res: any) => {
       this.loadData();
-    })
+    });
   }
 
-  public addToCart(){
+  public addToCart(): void{
     this.cart.addItem({
       name: this.product.name,
       description: this.product.description,
       price: this.selectedPrice,
-      quantity: this.selectedQuantity, 
+      quantity: this.selectedQuantity,
       photo: this.product.images[0].data.src,
       discount: this.product.discount,
       id: this.route.snapshot.params.id,
     });
-    this.layoutUtilsService.showSnack("success", "Product Added");
+    this.layoutUtilsService.showSnack('success', 'Product Added');
     this.layoutUtilsService.checkCartItemChange();
+  }
+
+  public onColorChange(c): void {
+    this.selectedColor = c.web_product_color_id;
   }
 }
